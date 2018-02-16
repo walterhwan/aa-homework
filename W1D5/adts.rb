cb30bacce9b87f4dc138232f5fe998df3e3fd67c
Walter Wan <WalterHWan@gmail.com>
2018-02-16 08:39:00 -0800

    Fix assign and added show in Map class

--- W1D5/adts.rb
+++ W1D5/adts.rb
@@ -23,6 +23,7 @@ class Queue
 
   def enqueue(el)
     @ivar << el
+    el
   end
 
   def dequeue
@@ -36,17 +37,17 @@ end
 
 class Map
   def initialize
-    @ivar = []
+    @map = []
   end
 
   def remove(key)
-    @ivar = @ivar.reject do |(k, _)|
+    @map.reject! do |(k, _)|
       k == key
     end
   end
 
   def lookup(key)
-    @ivar.each do |(k, v)|
+    @map.each do |(k, v)|
       return v if k == key
     end
 
@@ -54,7 +55,20 @@ class Map
   end
 
   def assign(key, value)
-    @ivar << [key, value]
+    idx = @map.index { |(k, v)| k == key }
+    idx ? @map[idx][1] = value : @map << [key, value]
+    [key, value]
   end
 
+  def show
+    dup_map(@map)
+  end
+
+  private
+
+  def dup_map(arr)
+    arr.map do |el|
+      el.is_a?(Array) ? dup_map(el) : el
+    end
+  end
 end
