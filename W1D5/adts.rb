18df6be1ed77a5d3b11fa791640a0466291f9562
Walter Wan <WalterHWan@gmail.com>
2018-02-16 08:27:26 -0800

    Add and finish Map class

--- W1D5/adts.rb
+++ W1D5/adts.rb
@@ -35,5 +35,38 @@ class Queue
 end
 
 class Map
-  
+  def initialize
+    @ivar = []
+  end
+
+  def remove(key)
+    @ivar = @ivar.reject do |(k, _)|
+      k == key
+    end
+  end
+
+  def lookup(key)
+    @ivar.each do |(k, v)|
+      return v if k == key
+    end
+
+    nil
+  end
+
+  def assign(key, value)
+    @ivar << [key, value]
+  end
+
 end
+
+m = Map.new
+m.assign('a', 1)
+m.assign('b', 2)
+m.assign('c', 3)
+
+p m.lookup('c')
+m.remove('b')
+m.remove('a')
+m.remove('c')
+
+p m
