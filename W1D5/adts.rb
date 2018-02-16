52a7b55c4a7ae613ac9918926e76e5f4f8f700c8
Walter Wan <WalterHWan@gmail.com>
2018-02-15 19:50:07 -0800

    Add Stack and Queue class

--- W1D5/adts.rb
+++ W1D5/adts.rb
@@ -1,17 +1,39 @@
 class Stack
   def initialize
-    # create ivar to store stack here!
+    @ivar = []
   end
 
   def add(el)
-    # adds an element to the stack
+    @ivar << el
   end
 
   def remove
-    # removes one element from the stack
+    @ivar.pop
   end
 
   def show
-    # return a copy of the stack
+    self.dup
   end
+end
+
+class Queue
+  def initialize
+    @ivar = []
+  end
+
+  def enqueue(el)
+    @ivar << el
+  end
+
+  def dequeue
+    @ivar.shift
+  end
+
+  def show
+    self.dup
+  end
+end
+
+class Map
+  
 end
