50114e8566aee5d4b13a2d507ad3b4fd194f6d0c
Walter Wan <WalterHWan@gmail.com>
2018-02-22 18:48:28 -0800

    Fix some unnecessary @storee calls

--- W2D5/LRUCache.rb
+++ W2D5/LRUCache.rb
@@ -16,11 +16,11 @@ class LRUCache
   end
 
   def count
-    @store.count
+    store.count
   end
 
   def show
-    @store.dup
+    store.dup
   end
 
   private
@@ -47,5 +47,5 @@ if $PROGRAM_NAME == __FILE__
   johnny_cache.add({a: 1, b: 2, c: 3})
 
 
-  johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
+  p johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
 end
