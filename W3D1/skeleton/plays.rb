69072a5b9205f75a09d6fe6a64a3e3e36fd23bda
Walter Wan <WalterHWan@gmail.com>
2018-02-26 22:21:46 -0800

    "Finish Playwright class"

--- W3D1/skeleton/plays.rb
+++ W3D1/skeleton/plays.rb
@@ -19,6 +19,34 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    data = PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        title = ?
+    SQL
+
+    Play.new(*data)
+  end
+
+  def self.find_by_playwright(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        plays
+      JOIN
+        playwrights ON plays.playwright_id = playwrights.id
+      WHERE
+        playwrights.name = ?
+    SQL
+
+    Play.new(*data)
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -48,4 +76,74 @@ class Play
         id = ?
     SQL
   end
+end
+
+class Playwright
+  attr_accessor :id, :name, :birth_year
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = ?
+    SQL
+
+    Playwright.new(*data)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+end
+
+
+if $PROGRAM_NAME == __FILE__
+  # p Play.all
+  # p Play.find_by_title('All My Sons')
+  # p Play.find_by_playwright('Eugene O\'Neill')
+
+  # p Playwright.all
+  # p Playwright.find_by_name('Arthur Miller')
+  # p playwright1 = Playwright.find_by_name('Eugene O\'Neill')
+  # p Playwright.new('name' => 'Walter', 'birth_year' => 1988)
+  # playwright1[0].birth_year = 1888
+
+  # playwright1[0].update
+  # p Playwright.all
+
+
 end
