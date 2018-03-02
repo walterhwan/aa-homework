aa5d33a07eaa8d337037b33ac39d5e0aa8414885
Walter Wan <WalterHWan@gmail.com>
2018-03-01 18:21:26 -0800

    Finish Metacorgis

--- W3D5/meta_corgis/meta_corgis.rb
+++ W3D5/meta_corgis/meta_corgis.rb
@@ -107,14 +107,29 @@ class MetaCorgiSnacks
   def initialize(snack_box, box_id)
     @snack_box = snack_box
     @box_id = box_id
+    snack_box.methods.grep(/^get_(.*)_info$/) { MetaCorgiSnacks.define_snack $1 }
   end
 
-  def method_missing(name, *args)
-    # Your code goes here...
-  end
+  # def method_missing(name, *args)
+  #   # Your code goes here...
+  #   if [:bone, :kibble, :treat].include?(name)
+  #     info = @snack_box.send("get_#{name}_info", @box_id)
+  #     tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+  #     result = "#{name.capitalize}: #{info}: #{tastiness}"
+  #     tastiness > 30 ? "* #{result}" : result
+  #   else
+  #     super
+  #   end
+  # end
 
 
   def self.define_snack(name)
-    # Your code goes here...
+    define_method(name) {
+      info = @snack_box.send("get_#{name}_info", @box_id)
+      tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+      cap_name = "#{name.split('_').map(&:capitalize).join(' ')}"
+      result = "#{cap_name}: #{info}: #{tastiness}"
+      tastiness > 30 ? "* #{result}" : result
+    }
   end
 end
