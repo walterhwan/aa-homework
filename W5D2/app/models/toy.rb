cd1dc49831a5d885b44881c425b5021dfeec8909
Walter Wan <WalterHWan@gmail.com>
2018-03-12 21:40:24 -0700

    Complete

--- W5D2/app/models/toy.rb
+++ W5D2/app/models/toy.rb
@@ -1,4 +1,18 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :integer          not null, primary key
+#  name         :string           not null
+#  toyable_type :string
+#  toyable_id   :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Toy < ApplicationRecord
   validates :name, uniqueness: { scope: [:toyable] }
   belongs_to :toyable, polymorphic: true
+
+
 end
--- W5D2/test/models/toy_test.rb
+++ W5D2/test/models/toy_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :integer          not null, primary key
+#  name         :string           not null
+#  toyable_type :string
+#  toyable_id   :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class ToyTest < ActiveSupport::TestCase
