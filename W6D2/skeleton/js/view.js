f9e3f99daa5cf4f866b2cde923650b3e7f0d23c9
Walter Wan <WalterHWan@gmail.com>
2018-03-20 08:58:13 -0700

    Finish exercise4

--- W6D2/skeleton/js/view.js
+++ W6D2/skeleton/js/view.js
@@ -23,6 +23,7 @@ View.prototype.exercise1 = function () {
   //Result: Every square should turn orange (we already have a CSS rule)
 
   //your code here!
+  $('li').addClass("orange");
 };
 
 View.prototype.exercise2 = function () {
@@ -30,6 +31,7 @@ View.prototype.exercise2 = function () {
   //Result: Every square vanishes
 
   //your code here!
+  $('li').remove();
 };
 
 View.prototype.exercise3 = function () {
@@ -37,6 +39,7 @@ View.prototype.exercise3 = function () {
   //Result: An <h1> with the text 'i love jquery' appears under the grid.
 
   //your code here!
+  $('body').append('<h1>I love jQuery</h1>');
 };
 
 View.prototype.exercise4 = function () {
@@ -44,6 +47,10 @@ View.prototype.exercise4 = function () {
   //Result: Your name appears in every other square.
 
   //your code here!
+  let $squares = $('li');
+  for (let i = 0; i < $squares.length; i += 2) {
+    $squares[i].addClass('orange');
+  }
 };
 
 View.prototype.exercise5 = function () {
