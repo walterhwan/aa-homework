8d0373b209debae4946f391e390629a210cb0160
Walter Wan <WalterHWan@gmail.com>
2018-03-23 08:49:32 -0700

    Finish W6D5 homework

--- W6D3/js/index.js
+++ W6D3/js/index.js
@@ -2,16 +2,16 @@ console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
 $.ajax({
-     type: 'GET',
-     url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=b3f3afe7c950012492df79e0cfccf03c",
-     success(data) {
-       console.log("We have your weather!");
-       console.log(data);
-     },
-     error() {
-       console.error("An error occurred.");
-     },
-  });
+   type: 'GET',
+   url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=b3f3afe7c950012492df79e0cfccf03c",
+   success(data) {
+     console.log("We have your weather!");
+     console.log(data);
+   },
+   error() {
+     console.error("An error occurred.");
+   },
+});
 
 
 // Add another console log here, outside your AJAX request
