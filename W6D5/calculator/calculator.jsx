26008735af5a23b5bc7c67801e97a7a09851d7a6
Walter Wan <WalterHWan@gmail.com>
2018-03-25 10:17:07 -0700

    Add w7d1

---
+++ W6D5/calculator/calculator.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    // your code here
+  }
+
+
+
+  render() { return (
+    <div>
+      <h1>Hello World</h1>
+      <button></button>
+    </div>
+  );}
+}
+
+export default Calculator;
